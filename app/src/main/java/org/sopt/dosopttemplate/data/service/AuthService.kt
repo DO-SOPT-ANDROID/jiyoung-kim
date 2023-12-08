bc85db3dd160e164751908f1a055e1428d12ee4d
Cat-JiYoung <kimyamae82@gmail.com>
2023-12-08 16:06:13 +0900

    [FEAT] 회원가입 오류시 경고문구 / 버튼 비활성화 구현

--- app/src/main/java/org/sopt/dosopttemplate/data/service/AuthService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/service/AuthService.kt
@@ -2,9 +2,12 @@ package org.sopt.dosopttemplate.data.service
 
 import org.sopt.dosopttemplate.data.model.request.RequestSignInDto
 import org.sopt.dosopttemplate.data.model.request.RequestSignUpDto
+import org.sopt.dosopttemplate.data.model.response.ResponseGetUserDto
 import org.sopt.dosopttemplate.data.model.response.ResponseSignInDto
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AuthService {
     @POST("api/v1/members")
@@ -16,4 +19,9 @@ interface AuthService {
     suspend fun signIn(
         @Body requestSignInDto: RequestSignInDto,
     ): ResponseSignInDto
+
+    @GET("api/v1/members/{memberId}")
+    suspend fun getUser(
+        @Path("memberId") memberId: Int,
+    ): ResponseGetUserDto
 }
--- app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignupViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignupViewModel.kt
@@ -1,37 +1,65 @@
 package org.sopt.dosopttemplate.presentation.auth
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import org.sopt.dosopttemplate.domain.entity.User
 import org.sopt.dosopttemplate.domain.repository.AuthRepository
 import org.sopt.dosopttemplate.util.UiState
+import org.sopt.dosopttemplate.util.extension.addSourceList
 import javax.inject.Inject
 
 @HiltViewModel
 class SignupViewModel @Inject constructor(private val authRepository: AuthRepository) :
     ViewModel() {
+    val id: MutableLiveData<String> = MutableLiveData("")
+    val pwd: MutableLiveData<String> = MutableLiveData("")
+    val name: MutableLiveData<String> = MutableLiveData()
+    val mbti: MutableLiveData<String> = MutableLiveData()
+    val birthday: MutableLiveData<String> = MutableLiveData()
+
     private val _signUpState = MutableLiveData<UiState>()
     val signUpState: LiveData<UiState> get() = _signUpState
 
-    private val _isSignUpValid = MutableLiveData<Boolean>()
+    private val _isSignUpValid = MutableLiveData(false)
     val isSignUpValid: LiveData<Boolean> get() = _isSignUpValid
 
-    private fun checkIdValid(id: String): Boolean = id.length in 6..10 && id.isNotBlank()
+    private fun checkIdValid(id: String): Boolean = id.matches(ID_PATTERN) || id.isEmpty()
     private fun checkPwdValid(pwd: String): Boolean =
-        pwd.length in 8..12 && pwd.isNotBlank()
+        pwd.matches(PWD_PATTERN) || pwd.isEmpty()
 
-    private fun checkNameValid(name: String): Boolean = name.isNotEmpty()
+    val isIdValid: LiveData<Boolean> = id.map { id -> checkIdValid(id) }
+    val isPwdValid: LiveData<Boolean> = pwd.map { pwd -> checkPwdValid(pwd) }
 
-    private fun checkMbtiValid(mbti: String): Boolean = mbti.isNotEmpty()
+    init {
+        checkIdValid(id.value.toString())
+        checkPwdValid(pwd.value.toString())
+    }
 
-    fun signUp(user: User) {
+    private fun checkIsSignUpValid(): Boolean {
+        _isSignUpValid.value =
+            checkIdValid(id.value.toString()) && checkPwdValid(pwd.value.toString()) && !name.value.isNullOrEmpty() && !mbti.value.isNullOrEmpty() && !birthday.value.isNullOrEmpty()
+        Log.e("signup", "_isSignUpValid.value:: ${_isSignUpValid.value}")
+        return _isSignUpValid.value ?: false
+    }
+
+    val isEnabledSignUpBtn = MediatorLiveData<Boolean>().apply {
+        addSourceList(id, pwd, name, mbti, birthday) { checkIsSignUpValid() }
+    }
+
+    fun signUp() {
         viewModelScope.launch {
             runCatching {
-                authRepository.signUp(user.id, user.name, user.pwd)
+                authRepository.signUp(
+                    id.value.toString(),
+                    name.value.toString(),
+                    pwd.value.toString(),
+                )
             }.onSuccess {
                 _signUpState.value = UiState.Success
             }.onFailure {
@@ -45,5 +73,7 @@ class SignupViewModel @Inject constructor(private val authRepository: AuthReposi
         const val PWD_ERROR = "pwdError"
         const val EMPTY_ERROR = "emptyError"
         const val ERROR = "Error"
+        val ID_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,10}\$".toRegex()
+        val PWD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{6,12}\$".toRegex()
     }
 }
